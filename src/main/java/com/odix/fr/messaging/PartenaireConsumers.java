9bce74f213410ca45348fd430b5f069df1f11e2d
GitLabOdix <azaheni@smartgraphe.com>
2019-12-18 12:23:24 +0100

    Refactoring MS

---
+++ src/main/java/com/odix/fr/messaging/PartenaireConsumers.java
@@ -0,0 +1,35 @@
+package com.odix.fr.messaging;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.odix.fr.model.Partenaire;
+import com.odix.fr.service.PartenaireService;
+
+@Service
+public class PartenaireConsumers {
+
+	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+	
+	@Autowired
+	PartenaireService partenaireService;
+	
+    @KafkaListener(topics = "add-partenaire-topic")
+    public void addPartenaireConsumer(String message) throws IOException {
+        System.out.print(String.format("#### -> addPartenaireConsumer : Opportunite-Service -> %s", message +"\n"));
+
+        try{
+        	OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        	Partenaire partenaire = OBJECT_MAPPER.readValue(message, Partenaire.class);
+        	
+            this.partenaireService.addPartenaire(partenaire);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
